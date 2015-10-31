fe4c80fd4378f44b50c6655e970d61ea3ea9cf25
dmmarcus <dmmarcus1@gmail.com>
2015-10-31 04:09:28 -0700

    Update InsertionSort.java

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,58 +1,20 @@
 public class InsertionSort {
 	
-	
-	public static void main (String[] args){
+  public static void sort(int[] num){
 		
-		int[] num = new int[args.length];
-		try{
-		
-			for (int i = 0; i < args.length; ++i){
+    for (int j = 1; j < num.length; ++j){
 			
-				num[i] = Integer.parseInt(args[i]);
+      int key = num[j];
+      int i = j - 1;
+			
+      while (i >= 0 && num[i] > key){
 				
-			}
-			
-		} 
+        num[i+1] = num[i];
+        i = i - 1;
+      }
+    num[i+1] = key;
+   }
 		
-		catch (Exception e){
-			System.exit(-1);
-		}
-		
-		for (int i = 0; i < num.length; ++i){
-			
-			System.out.print(num[i] + " ");
-			
-		}
-		
-		sort(num);
-		
-		System.out.println();
-		
-		for (int i = 0; i < num.length; ++i){
-			
-			System.out.print(num[i] + " ");
-			
-		}
-		
-		
-		
-	}
-	
-	public static void sort(int[] num){
-		
-		for (int j = 1; j < num.length; ++j){
-			
-			int key = num[j];
-			int i = j - 1;
-			
-			while (i >= 0 && num[i] > key){
-				
-				num[i+1] = num[i];
-				i = i - 1;
-			}
-			num[i+1] = key;
-		}
-		
-		return;
-	}
+   return;
+  }
 }
